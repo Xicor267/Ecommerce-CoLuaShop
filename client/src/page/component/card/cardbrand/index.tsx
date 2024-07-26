7ecd3b56f6f14f334f09e21f517a918c82b6fc65
Nolan.Nguyen <nolan.nguyen@avepoint.com>
2024-07-27 01:06:04 +0700

    [COLUASHOP-7] add new grid, list layout and map

--- client/src/page/component/card/cardbrand/index.tsx
+++ client/src/page/component/card/cardbrand/index.tsx
@@ -14,7 +14,7 @@ export const CardBrand: FunctionComponent<ICardBrand> = (props) => {
     useEffect(() => {
         const intervalId = setInterval(() => {
             handleNextClick();
-        },4000);
+        }, 4000);
 
         return () => clearInterval(intervalId);
     }, []);
@@ -29,26 +29,31 @@ export const CardBrand: FunctionComponent<ICardBrand> = (props) => {
 
     const startIndex = currentPage;
     const endIndex = (startIndex + props.perPage) % props.item.length;
-    
-    const currentItem = startIndex < endIndex 
-        ? props.item.slice(startIndex, endIndex) 
-        : [...props.item.slice(startIndex), ...props.item.slice(0, endIndex)];
 
+    const currentItem = startIndex < endIndex
+        ? props.item.slice(startIndex, endIndex)
+        : [...props.item.slice(startIndex), ...props.item.slice(0, endIndex)];
 
     return (
         <div className="cardbrand-container">
             <div className="cardbrand-header">
                 <div className="cardbrand-buttons-title">{props.title}</div>
                 <div className="cardbrand-buttons">
-                    <CgArrowLeftR onClick={handlePrevClick} className="cardbrand-buttons-icon" fontSize={20}/>
-                    <CgArrowRightR onClick={handleNextClick} className="cardbrand-buttons-icon" fontSize={20}/>
+                    <CgArrowLeftR onClick={handlePrevClick} className="cardbrand-buttons-icon" fontSize={20} />
+                    <CgArrowRightR onClick={handleNextClick} className="cardbrand-buttons-icon" fontSize={20} />
                 </div>
             </div>
-            <div className="cardbrand-details">
-                {currentItem.map((item, index) => (
-                    <img key={index} className="cardbrand-image" src={item.image} />
-                ))}
+            <div className="cardbrand-slider">
+                <div className="cardbrand-details">
+                    {currentItem.map((item, index) => (
+                        <img key={index} className="cardbrand-image" src={item.image} />
+                    ))}
+                    {currentItem.map((item, index) => (
+                        <img key={`clone-${index}`} className="cardbrand-image" src={item.image} />
+                    ))}
+                </div>
             </div>
+
         </div>
     )
 }
